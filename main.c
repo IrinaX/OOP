670dff5f4e36a8bfb2da75fa7d289086d2fee99e
IrinaX <iksanova08@mail.ru>
2021-02-03 01:39:45 +0500

    1st lab done

--- main.c
+++ main.c
@@ -11,7 +11,7 @@ typedef struct List {
     struct Item *pHead;
 } list;
 
-void addListItem(item *listItem, list *myList);
+void addListItem(list *myList, item *listItem);
 
 void printList(list *myList);
 
@@ -20,6 +20,14 @@ int countListItems(list *myList);
 void printMenu();
 
 item *getItem(list *myList, int index);
+
+item *removeItem(list *myList, int index);
+
+void deleteItem(list *myList, int index);
+
+void clearList(list *myList);
+
+void insertItem(list *myList, item *newItem, int index);
 
 int main() {
     //creating empty list
@@ -32,28 +40,26 @@ int main() {
     myList->pHead = 0;
     myList->pTail = 0;
     int variant = 0;
-    while (variant <= 3) {
+    while (variant <= 7) {
         printMenu();
         scanf("%d", &variant);
         switch (variant) {
-            case 0: { //добавление элемента в хвост
-
+            case 0: { //Add item to tail
                 listItem = malloc(sizeof(item));
-
-                addListItem(listItem, myList);
+                addListItem(myList, listItem);
                 printf("\nItem was added successfully!\n");
                 break;
             }
-            case 1: {//вывод списка на экран
+            case 1: {//Show list
                 printList(myList);
                 break;
             }
-            case 2: {//подсчет кол-ва эл-ов в списке
+            case 2: {//Count items
                 int amountOfItems = countListItems(myList);
                 printf("\namountOfItems = %d\n", amountOfItems);
                 break;
             }
-            case 3: {//получение элемента по индексу
+            case 3: {//Get item by index
                 if (myList->pHead == NULL) {//если начальный эл-т отсутствует, то список пуст
                     printf("\nList is empty.\n");
                 } else {
@@ -68,9 +74,57 @@ int main() {
                 }
                 break;
             }
-            case 4: {//выход из программы
+            case 4: {//Insert item
+                if (myList->pHead == NULL) {//если начальный эл-т отсутствует, то список пуст
+                    printf("\nList is empty.\n");
+                } else {
+                    int index = 0;
+                    item *newItem = malloc(sizeof(item));
+                    printf("\nEnter index of element: ");
+                    scanf("%d", &index);
+                    insertItem(myList, newItem, index);
+                    printf("\nNew item was inserted successfully!\n");
+                }
+                break;
+            }
+            case 5: {//Remove item by index
+                if (myList->pHead == NULL) {//если начальный эл-т отсутствует, то список пуст
+                    printf("\nList is empty.\n");
+                } else {
+                    int index = 0;
+                    printf("\nEnter index of element: ");
+                    scanf("%d", &index);
+                    item *removedElement = removeItem(myList, index);
+                    if (removedElement != 0) {
+                        printf("\n#\tremovedItem\tpPrev\t\tpNext\n");
+                        printf("%d\t%p\t%p\t%p\n", index, removedElement, removedElement->pPrev, removedElement->pNext);
+                    }
+                }
+                break;
+            }
+            case 6: {//Delete item by index
+                if (myList->pHead == NULL) {//если начальный эл-т отсутствует, то список пуст
+                    printf("\nList is empty.\n");
+                } else {
+                    int index = 0;
+                    printf("\nEnter index of element: ");
+                    scanf("%d", &index);
+                    deleteItem(myList, index);
+                }
+                break;
+            }
+            case 7: {//Clear list
+                if (myList->pHead == NULL) {//если начальный эл-т отсутствует, то список пуст
+                    printf("\nList is empty.\n");
+                } else {
+                    clearList(myList);
+                }
+                break;
+            }
+            case 8: {//Exit
                 printf("Exit...");
                 if (myList != NULL) {
+                    clearList(myList);
                     free(myList);
                     myList = NULL;
                 }
@@ -89,7 +143,7 @@ int main() {
 }
 
 /* голова - <0===> - хвост */
-void addListItem(item *listItem, list *myList) {
+void addListItem(list *myList, item *listItem) {
     if (myList->pHead == NULL) {//если нет элементов в списке
         listItem->pPrev = NULL;//добавленный элемент ни на что не указывает
         listItem->pNext = NULL;
@@ -127,16 +181,11 @@ void printMenu() {
     printf("1. Show list\n");
     printf("2. Count items\n");
     printf("3. Get item by index\n");
-    printf("4. Exit\n");
-//    printf("4. Show notebook\n");
-//    printf("5. Show person info by his number in notebook\n");
-//    printf("6. Add person\n");
-//    printf("7. Remove person\n");
-//    printf("8. Save notebook\n");
-//    printf("9. Load notebook from file\n");
-//    printf("10. Delete file\n");
-//    printf("11. Clear all data\n");
-//    printf("12. Exit\n");
+    printf("4. Insert item\n");
+    printf("5. Remove item by index\n");
+    printf("6. Delete item by index\n");
+    printf("7. Clear list\n");
+    printf("8. Exit\n");
     printf(">");
 }
 
@@ -156,4 +205,79 @@ item *getItem(list *myList, int index) {
     }
     return element;
 }
+
+void insertItem(list *myList, item *newItem, int index) {
+    item *element = getItem(myList, index);
+    if (element != 0) {
+        if (element == myList->pHead) { //если выбран 0-ой эл-т
+            newItem->pPrev = NULL;//у нового элемента обнуляем указатель на предыдущий элемент
+            newItem->pNext = element;// у newItem присваиваем указателю на след элемент адрес головы
+            element->pPrev = newItem;// у головы обновляем указатель на пред эл-т
+            myList->pHead = newItem;//обновляем указатель на голову)))
+            return;
+        }
+        if (element == myList->pTail) { //если выбран ласт эл-т
+            newItem->pNext = NULL;//у нового элемента обнуляем указатель на след элемент
+            newItem->pPrev = element;// у newItem присваиваем указателю на пред элемент адрес хвоста
+            element->pNext = newItem;// у хвоста обновляем указатель на след эл-т
+            myList->pTail = newItem;//обновляем указатель на хвост
+        } else {
+            element->pPrev->pNext = newItem;// у элемента index-1 присваиваем указателю на след элемент адрес newItem
+            newItem->pPrev = element->pPrev;// у newItem присваиваем указателю на пред элемент адрес элемента index-1
+            newItem->pNext = element; // у newItem присваиваем указателю на след элемент адрес элемента index
+            element->pPrev = newItem; // у элемента index присваиваем указателю на пред элемент адрес элемента newItem
+        }
+    }
+}
+
+item *removeItem(list *myList, int index) {
+    item *element = getItem(myList, index);
+    if (element == 0) {
+        return 0;
+    }
+    if (myList->pTail == myList->pHead) {//если в списке остался один элемент
+        myList->pTail = NULL;//хвост обнуляем
+        myList->pHead = NULL;//голову тоже
+//        printf("\nList is empty.\n");
+        return element;
+    }
+    if (element->pPrev == NULL) {//если удалить надо начальный элемент(голову) (индекс 0)
+        element->pNext->pPrev = NULL;//обнуляем указатель на голову
+        myList->pHead = element->pNext;//новая голова это эл-т с индексом 1
+    } else {
+        if (element->pNext == NULL) {//если удалить надо конечный элемент(хвост) (индекс n)
+            element->pPrev->pNext = NULL;//обнуляем указатель на хвост
+            myList->pTail = element->pPrev;//новый хвост это эл-т с индексом n-1
+        } else {//если элемент находится посередине
+            element->pPrev->pNext = element->pNext;//обновляем указатель на след элемент у пред элемента
+            element->pNext->pPrev = element->pPrev;//обновляем указатель на пред элемент у след элемента
+        }
+    }
+    return element;
+}
+
+void deleteItem(list *myList, int index) {
+    item *removedItem = removeItem(myList, index);
+    if (removedItem == 0) {
+        free(removedItem);
+        removedItem = NULL;
+        return;
+    }
+    free(removedItem);
+    removedItem = NULL;
+    printf("\nItem was successfully deleted!\n");
+}
+
+void clearList(list *myList) {
+    item *element = myList->pHead;//определяем начальный элемент списка для цикла
+    while (element != NULL) {
+        item *nextItem = element->pNext;//сохраняем указатель на следующий элемент
+        free(element);//освобождаем память
+        element = nextItem;//присваиваем текущему указателю сохраненный
+    }
+    myList->pHead = NULL;
+    myList->pTail = NULL;
+    printf("\nList was cleared successfully!\n");
+}
+
 
